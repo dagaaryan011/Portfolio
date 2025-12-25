e3598754f2c0831a9d78237dca35c1a7018f001c
dagaaryan011 <dagaaryan11@gmail.com>
2025-12-26 00:53:57 +0530

    Initial ver 1.4  portfolio commit - font color , scrolling issue fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+   
+    if (window.location.hash) {
+      window.history.replaceState(null, "", window.location.pathname);
+    }
+
+    
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
